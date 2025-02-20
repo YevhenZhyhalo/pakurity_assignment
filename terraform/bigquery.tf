a2de76c83a62e04618ed92793752a976cf9699bb
Yevhen Zhyhalo <yzhyhalo@MacBook-Pro-von-MacBook.local>
2025-02-20 04:02:56 +0200

    fix

--- terraform/bigquery.tf
+++
@@ -1,9 +0,0 @@
-module "data_warehouse" {
-  source  = "terraform-google-modules/bigquery/google//modules/data_warehouse"
-  version = "~> 9.0"
-
-  project_id          = var.project_id
-  region              = var.region
-  deletion_protection = false
-  force_destroy       = true
-}
