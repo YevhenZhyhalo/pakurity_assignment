e2a60fad868ee911815784aa912427216ca974c5
Yevhen Zhyhalo <yzhyhalo@MacBook-Pro-von-MacBook.local>
2025-03-02 21:31:03 +0200

    fixed script

--- script.py
+++ script.py
@@ -1,21 +1,21 @@
 
 
 query = """
-select id, REGEXP_REPLACE(report,r",","") as report 
+select id, REGEXP_REPLACE(report,r",","") as report
 from(
-select     
+select
   STRING_AGG(report) AS report,
   id
 from(
-  SELECT 
+  SELECT
     `timestamp` as TM,
     resource.labels.pod_name as id,
     textPayload AS report,
   FROM `spheric-shield-451012-u9.Trivy.stdout`
   Order by  TM
 )
-  GROUP BY id 
-) where id  LIKE "%scan-vulnerabilityreport%" 
+  GROUP BY id
+) where id  LIKE "%scan-vulnerabilityreport%"
 """
 
 
@@ -33,8 +33,9 @@ table_id = "filtered"
 destination_table = project_id+"."+dataset_id+"."+table_id
 
 schema = [
-    bigquery.SchemaField("id", "STRING", mode="REQUIRED"),
-    bigquery.SchemaField("json", "JSON", mode="NULLABLE"),
+    bigquery.SchemaField("scan_id", "STRING", mode="REQUIRED"),
+    bigquery.SchemaField("full_json", "JSON", mode="NULLABLE"),
+    bigquery.SchemaField("vulnerability", "JSON", mode="NULLABLE"),
 ]
 
 def query_data():
@@ -59,7 +60,7 @@ def load_data():
   table = dataset.table(table_id)
 
   rows = query_data()
-  df = pd.DataFrame(columns=['id', 'json'])
+  df = pd.DataFrame(columns=['scan_id', 'full_json','vulnerability'])
   print(df)
 
   for row in rows:
@@ -68,10 +69,13 @@ def load_data():
         bytes = base64.b64decode(row.report)
         data = bz2.decompress(bytes)
         js = json.loads(data)
-        ### Create data to load
-        new_row = pd.DataFrame([[row.id, js]],
-        columns=['id', 'json'])
-        df = pd.concat([df, new_row], ignore_index=True)
+        print("before")
+        for obj in  js["Results"]:
+          for vuln in obj["Vulnerabilities"]:
+            ### Create data to load
+            new_row = pd.DataFrame([[row.id, js,vuln]],
+            columns=['scan_id', 'full_json','vulnerability'])
+            df = pd.concat([df, new_row], ignore_index=True)
       except:
         print("error")
   print(df)
@@ -80,9 +84,7 @@ def load_data():
   job_config.schema = schema
   json_data = df.to_json(orient = 'records')
   json_object = json.loads(json_data)
-
   job = writer.load_table_from_json(json_object, table, job_config = job_config)
   print(job.result())
 
 load_data()
-    
